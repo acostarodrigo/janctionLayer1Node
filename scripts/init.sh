a8b5f47b8e107fd65f56e35d13adf19444e49897
Rodrigo Acosta <acosta_rodrigo@hotmail.com>
2025-01-26 18:12:35 -0400

    increased initial amount of token to alice

--- scripts/init.sh
+++ scripts/init.sh
@@ -19,10 +19,10 @@ $MINID_BIN keys add alice
 $MINID_BIN keys add bob
 $MINID_BIN init test --chain-id demo --default-denom mini
 # update genesis
-$MINID_BIN genesis add-genesis-account alice 10000000mini --keyring-backend test
+$MINID_BIN genesis add-genesis-account alice 100000000000mini --keyring-backend test
 $MINID_BIN genesis add-genesis-account bob 1000mini --keyring-backend test
 # create default validator
-$MINID_BIN genesis gentx alice 1000000mini --chain-id demo
+$MINID_BIN genesis gentx alice 100000000000mini --chain-id demo
 $MINID_BIN genesis collect-gentxs
 # video Rendering settings
 $MINID_BIN videoRendering enable true
